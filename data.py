20b49dfa46990856f54f307c07de7417e7f48343
OptimusPrime <maciejblaszczyk1996@gmail.com>
2017-06-05 16:58:04 +0200

    PCA added, getting data changed a bit

--- data.py
+++ data.py
@@ -1,6 +1,8 @@
 # This module will prepare data for our project
 import numpy as np
 import pandas
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
 
 # loading and reshaping data
 samples = 17898
@@ -12,19 +14,44 @@ classes = np.array(data[:, 8].astype(float))
 dataset = np.array(data[:, 0:8].astype(float))
 dataset = (dataset - np.mean(dataset, axis=0)) / np.std(dataset, axis=0)
 
-train_data_length = int(0.7*samples)
-test_data_length = samples - train_data_length
-
-
-train_data = dataset[:train_data_length]
-test_data = dataset[train_data_length:]
-train_labels = classes[:train_data_length]
-test_labels = classes[train_data_length:]
-
 
 def get_pulsar_data():
     """ It returns train_data, test_data, train_labels, test_labels """
+    train_data_length = int(0.7*samples)
+    test_data_length = samples - train_data_length
+
+    train_data = dataset[:train_data_length]
+    test_data = dataset[train_data_length:]
+    train_labels = classes[:train_data_length]
+    test_labels = classes[train_data_length:]
     return train_data, test_data, train_labels, test_labels
+
+
+def get_pulsar_data_reduced_to(n_features):
+    """ It returns train_data, test_data, train_labels, test_labels reduced to n_features by PCA"""
+    pca = PCA(n_components=n_features)
+    datasetPCA = pca.fit_transform(dataset)
+    train_data_length = int(0.7 * samples)
+    test_data_length = samples - train_data_length
+
+    train_data = datasetPCA[:train_data_length]
+    test_data = datasetPCA[train_data_length:]
+    train_labels = classes[:train_data_length]
+    test_labels = classes[train_data_length:]
+    return train_data, test_data, train_labels, test_labels
+
 
 def get_number_of_samples():
     return len(data)
+
+
+def show_data_in_2_dimensions(n_samples):
+    pca = PCA(n_components=2)
+    datasetPCA = pca.fit_transform(dataset)
+    x = np.zeros(len(datasetPCA))
+    y = np.zeros(len(datasetPCA))
+    for i in range(len(datasetPCA)):
+        x[i] = datasetPCA[i][0]
+        y[i] = datasetPCA[i][1]
+    plt.scatter(x[:n_samples], y[:n_samples], c=classes[:n_samples])
+    plt.show()
--- main.py
+++ main.py
@@ -9,10 +9,10 @@ import data
 import pandas as pd
 
 
-train_data, test_data, train_labels, test_labels = data.get_pulsar_data()
+train_data, test_data, train_labels, test_labels = data.get_pulsar_data_reduced_to(5)
 
 
-
+'''
 kernels_for_SVM = ['linear', 'poly', 'rbf', 'sigmoid']
 gammas_for_SVM = [0.000001, 0.0001, 0.001]
 coefs_for_SVM = [-1.0, 1.0, 0.0]
@@ -24,9 +24,9 @@ SVM_classification.perform()
 print(SVM_classification.get_sensitivity())
 print(SVM_classification.get_specificity())
 print(SVM_classification.get_precision())
+'''
 
-
-
+'''
 distance = ['euclidean', 'cityblock', 'minkowski']
 n_neighbours = [1, 2, 3, 5]
 weights = ['uniform', 'distance']
@@ -59,7 +59,7 @@ NN_classification.perform()
 print('\n NETWORK \n')
 print(NN_classification.get_general_results())
 print(NN_classification.get_sensitivity())
-
+'''
 
 
 trees = [5, 10, 20]
@@ -70,9 +70,9 @@ RFC_classification.perform()
 print(RFC_classification.get_sensitivity())
 print(RFC_classification.get_precision())
 print(RFC_classification.get_specificity())
+data.show_data_in_2_dimensions(15000)
 
-
-
+'''
 data = pd.DataFrame([
     ["SVM:"], SVM_classification.get_sensitivity(), SVM_classification.get_precision(), SVM_classification.get_specificity(),
     ["KNN:"], KNN_classification.get_sensitivity(), KNN_classification.get_precision(), KNN_classification.get_specificity(),
@@ -81,6 +81,6 @@ data = pd.DataFrame([
     ["LR:"], [LR_classification.get_sensitivity()], [LR_classification.get_precision()], [LR_classification.get_specificity()]
         ])
 data.to_csv("data.csv", index=False, header=False)
-
+'''
 
 
